f9860ebc0d8fe238f1c1b3915f4e69133a4f296c
DhirajMandal1216 <121878229+DhirajMandal1216@users.noreply.github.com>
2023-01-10 14:39:31 +0530

    Program to find a number is odd or even by taking user input.

--- src/main/com/java/practice/conditional_statement/OddOrEven.java
+++ src/main/com/java/practice/conditional_statement/OddOrEven.java
@@ -1,10 +1,11 @@
 package com.java.practice.conditional_statement;
-
+//Find a number is odd or even
 import java.util.Scanner;
 
 public class OddOrEven {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number:");
         int n=sc.nextInt();
         if(n%2==0){
             System.out.println(n+" Is even number");
