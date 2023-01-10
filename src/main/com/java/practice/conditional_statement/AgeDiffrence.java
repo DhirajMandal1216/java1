419f3ae8c24f32dc78fdcec1565b544cfa98dd1d
DhirajMandal1216 <121878229+DhirajMandal1216@users.noreply.github.com>
2023-01-10 14:41:07 +0530

    Program to find Person is Young or not Young.

---
+++ src/main/com/java/practice/conditional_statement/AgeDiffrence.java
@@ -0,0 +1,15 @@
+package com.java.practice.conditional_statement;
+import java.util.*;
+public class AgeDiffrence {
+    public static void main(String[]args){
+      Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the age:");
+      int age=sc.nextInt();
+      if(age>=14 && age<=55){
+          System.out.println("Person is young");
+      }
+      else{
+          System.out.println("Person is not young");
+      }
+    }
+}
