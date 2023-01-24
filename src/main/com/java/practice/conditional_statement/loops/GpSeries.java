8316c1d5b7f4d3e0431f2eae7e1576c06da9b14f
DhirajMandal1216 <121878229+DhirajMandal1216@users.noreply.github.com>
2023-01-24 09:15:47 +0530

    Program to Display Gp Series.

---
+++ src/main/com/java/practice/conditional_statement/loops/GpSeries.java
@@ -0,0 +1,24 @@
+package com.java.practice.conditional_statement.loops;
+
+import java.util.Scanner;
+
+public class GpSeries {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first number");
+        int a=sc.nextInt();
+        System.out.println("Enter common ratio");
+        int r=sc.nextInt();
+        System.out.println("Enter how many number of series you want = ");
+        int n=sc.nextInt();
+        int term=a;
+
+        for(int i=1;i<=n;i++){
+            System.out.print(term+" ,");
+            term=term*r;
+        }
+
+
+
+    }
+}
