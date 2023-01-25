db3ead6926f3a9e85ffb7092b931009745d63e09
DhirajMandal1216 <121878229+DhirajMandal1216@users.noreply.github.com>
2023-01-25 08:18:10 +0530

    Program to find greatest(max) element present in array.

--- src/main/com/java/practice/conditional_statement/arrays/MaxElement.java
+++ src/main/com/java/practice/conditional_statement/arrays/MaxElement.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class MaxElement {
     public static void main(String[] args) {
-        int A[]={3,9,7,8,12,6,15,5,4,10};
+        int A[]={3,9,7,8,12,6,15,5,4,10,11};
         int max=A[0];
         for(int i=0;i<A.length;i++){
             if(A[i]>max){
