58efad89017ba732c37d91c219c369eb10b723b0
DhirajMandal1216 <121878229+DhirajMandal1216@users.noreply.github.com>
2023-01-26 10:41:05 +0530

    Program to left rotation of an array.

--- src/main/com/java/practice/conditional_statement/arrays/LeftRotation.java
+++ src/main/com/java/practice/conditional_statement/arrays/LeftRotation.java
@@ -7,7 +7,7 @@ public class LeftRotation {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter number of element you want in array:");
         int n=sc.nextInt();
-        int A[]=new int[n];
+        int []A=new int[n];
         System.out.println("Enter array element:");
         for(int i=0;i<n;i++){
             A[i]=sc.nextInt();
@@ -42,13 +42,13 @@ public class LeftRotation {
 /*
 public class LeftRotation {
 public static void main(String[] args) {
-    int A[]={1,2,3,5,6,8,9,70};
+    int []A={1,2,3,5,6,8,9,70};
 
     //before rotation
         System.out.println("Before rotation");
     for(int i=0;i<A.length;i++){
         System.out.print(A[i]+" ");
-        System.out.println();
+
     }
     System.out.println();
     int first=A[0];
@@ -60,7 +60,7 @@ public static void main(String[] args) {
         System.out.println("After rotation");
     for(int i=0;i<A.length;i++){
         System.out.print(A[i]+" ");
-        System.out.println();
+
          }
   }
 
